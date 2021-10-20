4f595d2bba5c7da803217c43dff8e181b62e39b5
Nathalia Cristina <nathaliacristina20@hotmail.com>
2021-10-19 21:22:59 -0300

    first commit

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * LeftMenuFooter
+ *
+ */
+
+import React from 'react';
+import Wrapper from './Wrapper';
+
+function LeftMenuFooter() {
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <span>Mantido por</span>&nbspa;
+        <a key="website" href="https://reactavancado.com.br" target="_blank" rel="noopener noreferrer">
+          React Avançado
+        </a>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default LeftMenuFooter;
